0960c7fc4c2658b6860b3e66da78dae1a519602a
Edwin Piragauta Vargas <edwin.piragauta@gmail.com>
2020-11-06 11:38:53 -0500

    create variable name

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      string name = "Edwin";
+      Console.WriteLine("Hello World!" + name);
     }
   }
 }
